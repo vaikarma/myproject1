2dd20bba14dc51e17ba88c00847226ad724bbad1
vaikarma <vaibhavkarma@bestpeers.com>
2022-08-05 19:01:17 +0530

    can can

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,3 +13,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
+#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
+
+#!!!!
+
+#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
